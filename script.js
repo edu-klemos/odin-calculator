f9eca4fd703e1e68a9ad2ac1f4c9e35b2e3f0809
Eduardo <eduardo.klock@gmail.com>
2024-11-07 18:14:59 -0300

    Create function operate

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function subtract(a, b) { return a - b };
 function multiply(a, b) { return a * b };
 
 function divide(a, b) { return a / b };
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+            break;
+        case '-':
+            return subtract(firstNumber, secondNumber);
+            break;
+        case 'x':
+            return multiply(firstNumber, secondNumber);
+            break;
+        case '÷':
+            return divide(firstNumber, secondNumber);
+    }
+}
